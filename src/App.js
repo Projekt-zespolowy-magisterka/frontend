75e53bf5e2a933fc97123142a6c7cd0daa27bd32
AdamDomachowski1 <adam.domachowskig6@gmail.com>
2024-11-06 19:54:47 +0100

    Created Top Menu with logo, Added "useState" to App to set if user is logged or not

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/LoginPage';
-import RegistrationPage from './pages/RegistrationPage';
+import RegistrationPage from "./pages/RegistrationPage";
+
 import TopMenu from './components/TopMenu';
 import StockOverview from './pages/StockOverview';
 import Watchlist from './pages/Watchlist';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [username, setUsername] = useState('User123'); // Replace with actual user data
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [username, setUsername] = useState('test@gmail.com'); // Replace with actual user data
 
   const handleLogout = () => {
     setIsLoggedIn(false);
@@ -17,8 +18,8 @@ function App() {
 
   return (
     <Router>
-      {/* TopMenu is now always visible */}
-      <TopMenu username={username} isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+      {/* Render TopMenu only if the user is logged in */}
+      {isLoggedIn && <TopMenu username={username} isLoggedIn={isLoggedIn} onLogout={handleLogout} />}
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login onLogin={() => setIsLoggedIn(true)} />} />
